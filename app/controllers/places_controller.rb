3186405054f9f2436fd3a17beff6e9a3e90c1bec
Vasily Toropchin <vasview@gmail.com>
2016-12-17 19:04:19 +0600

    #7 Trying to add pictures

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,12 +2,14 @@ class PlacesController < ApplicationController
 
   def new
   	@place = Place.new
+  	@image = @place.images.build
   end
 
   def create
   	@place = Place.new(place_params)
-
+  	@image = Image.create(params[:image])
   	if @place.save
+  		binding.pry
   		flash[:success] = "Your message is being moderated, it will be added as soon as Administrator appoves it!"
   		redirect_to root_path
   	else
